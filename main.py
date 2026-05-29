033cf39e81247ac2f93804c628afc14423bbc7bd
William Jan <williamjan@MacBook-Pro-de-William.local>
2026-05-30 00:46:44 +0200

    fixe

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import sys
+import pathlib
 import time
 import torch
 import numpy as np
 import random
+
+ROOT = pathlib.Path(__file__).resolve().parent
+if str(ROOT) not in sys.path:
+    sys.path.insert(0, str(ROOT))
 
 import dagshub
 import mlflow
